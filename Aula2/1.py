5ae9f6c6a572f10ab29fb053be1587c407c2afcc
João Cândido Messias <159557724+Joao-candido-messias@users.noreply.github.com>
2026-03-02 22:57:37 +0000

    Feat: Implementação e teste das funções descritas em minha_biblioteca.py.

--- Aula2/1.py
+++ Aula2/1.py
@@ -1,38 +1,19 @@
-#Operações dobre lista (append), percorrer (for), remover ou deletar (remove)
+from minha_biblioteca import popular_aleatorio, popular_de_arquivo, exibir, copiar_lista_sem_replicados
 
-def popular_aleatorio(lista:int , quantidade:int):
-    """_summary_
-    Método que recebe uma lista e a popula com números aleatórios.
-    
-    Args:
-        lista (_type_): Lista de números.
-        quantidade (_type_): Quantidade de números a serem gerados.
-    """
+lista_numerosA= []
+lista_numerosB = []
 
+popular_aleatorio(lista_numerosA, 100)
+exibir(lista_numerosA)
 
-    def popular_de_arquivo(lista, nome_arquivo):
-        """_summary_
-        Método que recebe uma lista e a popula com dados vindo de um arquivo.
-        Args:
-            lista (_type_): Lista genérica
-            nome_arquivo (_type_): Nome do arquivo fonte que contém os dados
-        """
-    
-    def exibir(lista):
-        """_summary_
-        Método que recebe uma lista genérica, exibe todos os seus elementos na tela(um abaixo do outro) e 
-        no final exibe a quantidade de elementos  da lista
-        Args:
-            lista (_type_): Lista genérica
-        """
+# nome_arquivo = 'numeros.txt'
 
-    def copiar_lista_sem_replicados(lista_origem, lista_destino):
-        """_sumary_
-        Método que recebe uma lista de origem e copia todos os seus elementos para a lista destino, retirando os replicados
-        Args:
-            lista_origem(_type_): Lista com dados de origem
-            lista_destino(_type): Lista para receber os dados tradados 
-        
-        """       
+# popular_de_arquivo(lista_numerosB, nome_arquivo)
 
-    
+# exibir(lista_numerosB)
+
+lista_resultado = []
+
+copiar_lista_sem_replicados(lista_numerosA, lista_resultado)
+
+exibir(lista_resultado)
--- Aula2/minha_biblioteca.py
+++ Aula2/minha_biblioteca.py
@@ -1,17 +1,55 @@
-for minha_biblioteca import popular_aleatorio, popular_de_arquivo, exibir, copiar_lista_sem_replicados
+#Operações dobre lista (append), percorrer (for), remover ou deletar (remove)
+from  random import randint
 
-lista_numerosA= []
-lista_numerosB = []
 
-popular_aleatorio(lista_numerosA, 100)
+def popular_aleatorio(lista:int , quantidade:int):
+    """_summary_
+    Método que recebe uma lista e a popula com números aleatórios.
+    
+    Args:
+        lista (_type_): Lista de números.
+        quantidade (_type_): Quantidade de números a serem gerados.
+    """
+    
+    for i in range(quantidade):
+        lista.append(randint(0,100))
 
-nome_arquivo = 'numeros.txt'
 
-popular_de_arquivo (lista_numerosB, nome_arquivo)
+def popular_de_arquivo(lista, nome_arquivo):
+    """_summary_
+    Método que recebe uma lista e a popula com dados vindo de um arquivo.
+    Args:
+        lista (_type_): Lista genérica
+    nome_arquivo (_type_): Nome do arquivo fonte que contém os dados
+        """
+    with open(nome_arquivo, 'r', encoding = 'utf8') as leitor:
+        for linha in leitor:
+            lista.append(linha.strip())
+            
+    
+def exibir(lista):
+    """_summary_
+    Método que recebe uma lista genérica, exibe todos os seus elementos na tela(um abaixo do outro) e 
+    no final exibe a quantidade de elementos  da lista
+    Args:
+        lista (_type_): Lista genérica
+    """
+    for i in lista:
+        print(i)
+        print("----------------")
+        print("Total de registros: ", len(lista))
+        
 
-exibir(lista_numerosA)
-exibir(lista_numerosB)
+def copiar_lista_sem_replicados(lista_origem, lista_destino):
+    """_summary_
+    Função que recebe uma lista de origem e copia seus elementos para uma lista destino, removendo as duplicatas
+    Args:
+        lista_origem (_type_): Lista com os dados de origem
+        lista_destino (_type_): Lista para receber os dados tratados
+    """
+    for i in lista_origem:
+        if i not in lista_destino:
+            lista_destino.append(i)
+       
 
-lista_resultado = []
-
-copiar_lista_sem_replicados(lista_numerosB, lista_resultado)
+    
