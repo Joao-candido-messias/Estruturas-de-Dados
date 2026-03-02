5e0274baf32acd6b66cec826c00768692a5781ce
João Cândido Messias <159557724+Joao-candido-messias@users.noreply.github.com>
2026-03-02 22:04:47 +0000

    Docs: Adção de documentação de cada uma das funções de minha_biblioteca.py.

--- Aula2/1.py
+++ Aula2/1.py
@@ -1,1 +1,38 @@
 #Operações dobre lista (append), percorrer (for), remover ou deletar (remove)
+
+def popular_aleatorio(lista:int , quantidade:int):
+    """_summary_
+    Método que recebe uma lista e a popula com números aleatórios.
+    
+    Args:
+        lista (_type_): Lista de números.
+        quantidade (_type_): Quantidade de números a serem gerados.
+    """
+
+
+    def popular_de_arquivo(lista, nome_arquivo):
+        """_summary_
+        Método que recebe uma lista e a popula com dados vindo de um arquivo.
+        Args:
+            lista (_type_): Lista genérica
+            nome_arquivo (_type_): Nome do arquivo fonte que contém os dados
+        """
+    
+    def exibir(lista):
+        """_summary_
+        Método que recebe uma lista genérica, exibe todos os seus elementos na tela(um abaixo do outro) e 
+        no final exibe a quantidade de elementos  da lista
+        Args:
+            lista (_type_): Lista genérica
+        """
+
+    def copiar_lista_sem_replicados(lista_origem, lista_destino):
+        """_sumary_
+        Método que recebe uma lista de origem e copia todos os seus elementos para a lista destino, retirando os replicados
+        Args:
+            lista_origem(_type_): Lista com dados de origem
+            lista_destino(_type): Lista para receber os dados tradados 
+        
+        """       
+
+    
