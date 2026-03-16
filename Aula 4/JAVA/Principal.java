39fb394a3ddb3e427bf4ee93f9a30fd30b5134de
João Cândido Messias <159557724+Joao-candido-messias@users.noreply.github.com>
2026-03-16 22:04:21 +0000

    Feat: Primeiro exercicio da aula 4.

---
+++ Aula 4/JAVA/Principal.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Principal {
+    public static void main(String[] args) {
+        ArrayList<Aluno> alunos = new ArrayList<>();
+        
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Digite o nome do aluno: ");
+            String nome = scanner.nextLine().toUpperCase();
+            Aluno aluno = new Aluno(nome);
+            if (!alunos.contains(aluno)) {
+                alunos.add(aluno);
+            }
+        }
+        
+        for (Aluno aluno : alunos) {
+            System.out.println(aluno);
+        }
+    }
+}
+        
