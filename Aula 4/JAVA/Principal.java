647ee8458d38f1aca31b5a91563f9627bfda9faa
João Cândido Messias <159557724+Joao-candido-messias@users.noreply.github.com>
2026-03-16 22:56:18 +0000

    Feat: Criacção da classe RegistroClimatico em "minhaBiblioteca.java"

--- Aula 4/JAVA/Principal.java
+++ Aula 4/JAVA/Principal.java
@@ -1,13 +1,12 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class Principal {
     public static void main(String[] args) {
         ArrayList<Aluno> alunos = new ArrayList<>();
         
         Scanner scanner = new Scanner(System.in);
-        
+
         for (int i = 0; i < 3; i++) {
             System.out.print("Digite o nome do aluno: ");
             String nome = scanner.nextLine().toUpperCase();
