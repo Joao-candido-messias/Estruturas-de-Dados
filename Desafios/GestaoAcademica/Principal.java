4112579b7c9c9c8760d3831657adcb03be2faa6c
João Cândido Messias <159557724+Joao-candido-messias@users.noreply.github.com>
2026-05-01 23:57:57 +0000

    feat: adiciona busca de aluno via entrada do usuário com Scanner

--- Desafios/GestaoAcademica/Principal.java
+++ Desafios/GestaoAcademica/Principal.java
@@ -1,12 +1,17 @@
 package Desafios.GestaoAcademica;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) {
         try {
+            Scanner scanner = new Scanner(System.in);
             String caminho = "./Desafios/GestaoAcademica/DadosAlunos.csv";
             ArrayList<Aluno> alunos = Aluno.lerCsv(caminho);
-            System.out.println(Aluno.buscaAluno("Karen Duarte", alunos));
+            String nome;
+            System.out.println("Insira o nome que você deseja procurar: ");
+            nome = scanner.nextLine();
+            System.out.println(Aluno.buscaAluno(nome, alunos));
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
