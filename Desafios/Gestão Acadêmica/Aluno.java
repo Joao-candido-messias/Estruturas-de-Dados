afa1f34eeb2e56baddef25411ae45c9d2c1164ea
João Cândido Messias <159557724+Joao-candido-messias@users.noreply.github.com>
2026-05-01 17:17:24 +0000

    feat: Criar classe Aluno com construtor.

---
+++ Desafios/Gestão Acadêmica/Aluno.java
@@ -0,0 +1,13 @@
+public static class Aluno{
+    public String nome;
+    public String curso;
+    public String sexo; 
+    public int anoIngresso;
+
+    public Aluno(String nome, String curso, String sexo, int anoIngresso){
+        this.nome = nome;
+        this.curso = curso;
+        this.sexo = sexo;
+        this.anoIngresso = anoIngresso;
+    }
+}
