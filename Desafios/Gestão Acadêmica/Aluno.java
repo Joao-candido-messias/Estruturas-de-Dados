cb5c35bcf1796aa07184baa19be7cf07c823c099
João Cândido Messias <159557724+Joao-candido-messias@users.noreply.github.com>
2026-05-01 20:05:05 +0000

    feat: Criar a função lerCsv

--- Desafios/Gestão Acadêmica/Aluno.java
+++ Desafios/Gestão Acadêmica/Aluno.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class Aluno{
     private String nome;
     private String curso;
@@ -34,6 +38,47 @@ public class Aluno{
     //Getter de anoIngresso
     public int GetAnoIngresso(){
         return anoIngresso;
+    }
+
+
+    /**
+    * Função que lê um arquivo .csv e retorna uma lista de alunos
+    *@param caminho: Caminho do arquivo .csv
+    *@return: Lista de Alunos
+    *@throws IOException Se ocorrer erro na leitura do arquivo
+    */
+    public static ArrayList<Aluno> lerCsv(String caminho) throws IOException{
+        ArrayList<Aluno> alunos = new ArrayList<>();
+
+        try(BufferedReader br = new BufferedReader(new FileReader(caminho))){
+
+            //Ignora o cabeçalho
+            br.readLine();
+
+            String linha;
+
+            while((linha = br.readLine())  != null){
+                //Ignora linhas vazias
+                if(linha.trim().isEmpty()){
+                    continue;
+                }
+
+                String[] dados = linha.split(",");
+
+                //Garante que todos os dados tenham as 4 informações necessarias
+                if(dados.length < 4){
+                    continue;
+                }
+
+                String nome = dados[0];
+                String curso = dados[1];
+                String sexo = dados[2];
+                int anoIngresso = Integer.parseInt(dados[3]);
+
+                alunos.add(new Aluno(nome, curso, sexo, anoIngresso));
+            }
+        }
+        return alunos;
     }
 
     @Override
