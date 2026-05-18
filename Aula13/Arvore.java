d302406b2956714de896fd9d200019a161e05c31
João Cândido Messias <159557724+Joao-candido-messias@users.noreply.github.com>
2026-05-18 23:17:48 +0000

    feat: Finaliza os testes de Aula13

--- Aula13/Arvore.java
+++ Aula13/Arvore.java
@@ -4,7 +4,6 @@ public class Arvore{
     public static void main(String[]args){
         TreeSet<Integer> arvore = new TreeSet<>();
         arvore.add(5);
-        arvore.add(10);
         arvore.add(15);
         arvore.add(7);
         arvore.add(20);
@@ -14,8 +13,20 @@ public class Arvore{
 
         // System.out.println("Elementos da arvore: "+arvore);
 
-        for(Integer elemento : arvore){
+         for(Integer elemento : arvore){
             System.out.println("Elemento:"+elemento);
         }
+
+        if(arvore.contains(10)){
+            System.out.println("A arvore contem o elemento 10.");
+            arvore.remove(10);
+            System.out.println("Elemento 10 removido.");
+        }else{
+            System.out.println("A arvore não contem o elemento 10.");
+            arvore.add(10);
+            System.out.println("Elemento 10 inserido.");
+        }
+
+       
     }
 }
