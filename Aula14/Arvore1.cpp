93ab20b9422eed4cf6d0f0218eb7e5b268b6a7ff
João Cândido Messias <159557724+Joao-candido-messias@users.noreply.github.com>
2026-05-26 00:25:41 +0000

    feat: Implementa a segunda parte da Aula14

--- Aula14/Arvore1.cpp
+++ Aula14/Arvore1.cpp
@@ -32,6 +32,79 @@ void edr(Arvore *raiz){
     }
 }
 
+//Metodo que conta o número de nós de uma Arvore
+int contarNos(Arvore *raiz){
+    if(raiz){
+        return 1 + contarNos(raiz->esq) + contarNos(raiz->dir);
+    }
+    return 0;
+}
+
+//Metodo que conta o numero de folhas de uma arvore
+int contarFolhas(Arvore *raiz){
+    if(!raiz){
+        return 0;
+    }
+    if(!raiz->esq && !raiz->dir){
+        return 1;
+    }
+    return contarFolhas(raiz->esq) + contarFolhas(raiz->dir);
+}
+
+//Metodo que retorno o maior valor de uma Arvore
+int maior(Arvore *raiz){
+    if(!raiz) return -27;
+
+    Arvore *p;
+    for(p = raiz; p->dir; p = p->dir);
+    
+    return p->valor;
+}
+
+//Metodo que retorna o menor valor de uma Arvore
+int menor(Arvore *raiz){
+    if(!raiz) return -27;
+
+    Arvore *p;
+    for(p = raiz; p->esq; p = p->esq);
+
+    return p->valor;
+}
+
+//Metodo que encontra um valor em uma Arvore
+bool encontrou(int valor, Arvore *raiz){
+    if(raiz){
+        if(valor == raiz->valor){
+            return 1;
+        }
+        if(valor < raiz->valor){
+            return(encontrou(valor, raiz->esq));
+        }
+        if(valor > raiz->valor){
+            return(encontrou(valor, raiz->dir));
+        }
+    }
+    return 0;
+}
+
+//Metodo que devolve o nivel de um numero em uma Arvore
+int nivel(int valor, Arvore *raiz){
+    if(raiz){
+        if(valor == raiz->valor) return 0;
+        if(valor<raiz->valor){
+            int resp = nivel(valor, raiz->esq);
+            if(resp == -1) return -1;
+            return resp+1;
+        }else{
+            int resp = nivel(valor, raiz->dir);
+            if(resp == -1) return -1;
+            return resp+1;
+        }
+    }
+    return -1;
+}
+
+//Metodo que exibe uma Arvore
 void exibir(int nivel, Arvore * raiz){
     if(raiz){
         exibir(nivel+1, raiz->dir);
@@ -45,10 +118,12 @@ void exibir(int nivel, Arvore * raiz){
 }
 
 
-
 //Metodo inserir ordenado recursivo em arvore binaria de peaquisa = ORDENADO
 Arvore *inserir(int valor,Arvore *raiz){
     if(raiz){
+        //Controle de replicados
+        if(valor == raiz->valor) return raiz;
+
         if(valor < raiz->valor){
             raiz->esq = inserir(valor, raiz->esq);
         }else{
@@ -73,11 +148,11 @@ int main(){
         raiz = inserir(rand() % 30, raiz);
     }
 
-    raiz = inserir(100, raiz);
-    raiz = inserir(50, raiz);
-    raiz = inserir(150, raiz);
-    raiz = inserir(60, raiz);
-    raiz = inserir(80, raiz);
+    // raiz = inserir(100, raiz);
+    // raiz = inserir(50, raiz);
+    // raiz = inserir(150, raiz);
+    // raiz = inserir(60, raiz);
+    // raiz = inserir(80, raiz)
 
     // red(raiz);//Raiz esquerda direita
     // cout << "\n";
@@ -87,5 +162,18 @@ int main(){
 
     exibir(0, raiz);
 
-    return  0;
+    cout << "Quantidade de nós:" << contarNos(raiz) << ".\n";
+
+    cout << "Quantidade de folhas:" << contarFolhas(raiz) << ".\n";
+
+    cout << "Maior número:" << maior(raiz) << ".\n";
+
+    cout << "Menor número:" << menor(raiz) << ".\n";
+
+    int valor = 5;
+
+    cout << "Encontrou " << valor << "?" << encontrou(valor, raiz) << ".\n";
+
+    cout << "O numero " << valor << " esta no nivel " << nivel(valor, raiz) << ".\n";
+    return  0; 
 }
